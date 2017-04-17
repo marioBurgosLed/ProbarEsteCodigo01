23523c5f2222389b81d65c310eb011013b80a70a
marioBurgosLed <mario.burgos.led@gmail.com>
2017-04-17 18:02:16 -0300

    Initial commit

---
+++ app/src/main/java/co/com/smartgeeks/tutorial38/MainActivity.java
@@ -0,0 +1,91 @@
+package co.com.smartgeeks.tutorial38;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import static android.provider.ContactsContract.CommonDataKinds.Website.URL;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText etUsuario, etContra;
+    Button btnIngresar;
+    JSONArray ja;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        etUsuario = (EditText)findViewById(R.id.etUsuario);
+        etContra = (EditText)findViewById(R.id.etContra);
+        btnIngresar = (Button)findViewById(R.id.btnIngresar);
+
+        btnIngresar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                ConsultaPass("http://http://sigequip.esy.es/obtenerUsuariosCopia.php="+etUsuario.getText().toString());
+
+            }
+        });
+    }
+
+    private void ConsultaPass(String URL) {
+
+        Log.i("url",""+URL);
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+        StringRequest stringRequest =  new StringRequest(Request.Method.GET, URL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+
+                try {
+                    ja = new JSONArray(response);
+                    String contra = ja.getString(0);
+                    if(contra.equals(etContra.getText().toString())){
+
+                        Toast.makeText(getApplicationContext(),"Bienvenido",Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(MainActivity.this, pantallaBienvenido.class);
+                        startActivity(intent);
+
+                    }else{
+                        Toast.makeText(getApplicationContext(),"verifique su contraseña",Toast.LENGTH_SHORT).show();
+
+                    }
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+
+                    Toast.makeText(getApplicationContext(),"El usuario no existe en la base de datos",Toast.LENGTH_LONG).show();
+                }
+
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+
+        queue.add(stringRequest);
+
+
+
+    }
+}
